4191bdf1b82c2f80a6a11ceb984f83addb67d1d8
Peter Halloran <mkwenhua101@gmail.com>
2017-04-14 18:51:01 -0500

    fix: fix file export

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./src/dist/reactalike.js');
+  module.exports = require('./src/ex.js');
 } else {
-  module.exports = require('./src/dist/reactalike.js');
+  module.exports = require('./src/ex.js');
 }
